2177fd2923a2468c3fca3f34aad13e036e041b1e
TruongThuyQuangNhat <truongthuyquangnhat1@gmail.com>
2022-11-29 18:01:59 +0700

    setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NhanvienComponent } from './nhanvien/nhanvien.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'nhanvien', component: NhanvienComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {MatIconModule} from '@angular/material/icon';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { NhanvienComponent } from './nhanvien/nhanvien.component';
+import {MatTableModule} from '@angular/material/table';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NhanvienComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
